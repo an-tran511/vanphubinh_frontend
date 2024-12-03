5eefbfda4879d3124422c8d3f7674d3fd61e9276
An <an.tran511@outlook.com>
2024-12-03 09:57:10 +0700

    feat: set up query client

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,23 @@
 import './App.css'
+import { MutationCache, QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { RouterProvider, createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			refetchOnWindowFocus: false, // default: true
+			staleTime: 10000,
+		},
+	},
+	mutationCache: new MutationCache({
+		onSuccess: (_data, _variables, _context, mutation) => {
+			queryClient.invalidateQueries({
+				queryKey: mutation.options.mutationKey,
+			})
+		},
+	}),
+})
 
 declare module '@tanstack/react-router' {
 	interface Register {
@@ -9,15 +26,16 @@ declare module '@tanstack/react-router' {
 }
 const router = createRouter({
 	routeTree,
+	context: { queryClient },
 	defaultPreloadStaleTime: 0,
 	defaultPreload: 'intent',
 })
 
 function App() {
 	return (
-		<>
+		<QueryClientProvider client={queryClient}>
 			<RouterProvider router={router} />
-		</>
+		</QueryClientProvider>
 	)
 }
 
