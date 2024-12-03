f21d2597bec64c6dab9c2976615e34dcf18a6419
An <an.tran511@outlook.com>
2024-12-03 09:40:20 +0700

    feat: set up tanstack router

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,22 @@
-import { useState } from 'react'
-import viteLogo from '/vite.svg'
-import reactLogo from './assets/react.svg'
 import './App.css'
+import { RouterProvider, createRouter } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
+
+declare module '@tanstack/react-router' {
+	interface Register {
+		router: typeof router
+	}
+}
+const router = createRouter({
+	routeTree,
+	defaultPreloadStaleTime: 0,
+	defaultPreload: 'intent',
+})
 
 function App() {
-	const [count, setCount] = useState(0)
-
 	return (
 		<>
-			<div>
-				<a href="https://vite.dev" target="_blank" rel="noreferrer">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank" rel="noreferrer">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)} type="button">
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+			<RouterProvider router={router} />
 		</>
 	)
 }
